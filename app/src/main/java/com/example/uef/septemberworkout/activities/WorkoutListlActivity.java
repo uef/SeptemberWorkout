ad2acdee1892538024755cc39be87deb511c049d
uef <uef_kochemar@mail.ru>
2018-10-09 18:13:52 +0300

    Добавил активность со списком упражнений

---
+++ app/src/main/java/com/example/uef/septemberworkout/activities/WorkoutListlActivity.java
@@ -0,0 +1,15 @@
+package com.example.uef.septemberworkout.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.uef.septemberworkout.R;
+
+public class WorkoutListlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_listl);
+    }
+}
